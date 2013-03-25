de2351fdc2ab00808eb27f17c64aba7b1fc7034a
Luka Kosenina <luka.kosenina@gmail.com>
2013-03-25 17:35:29 +0100

    Init moj projekt.

--- tracker/settings.py
+++ tracker/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/Users/Tim/Tracker/database.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:/Users/Luka/Documents/GitHub/tld/database.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
