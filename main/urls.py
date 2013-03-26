c0072f2e27123e66e7293a83cd03be84b978d958
Luka Kosenina <luka.kosenina@gmail.com>
2013-03-26 20:36:44 +0100

    Views, okrnjena verzija 01.

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ from main import views
 urlpatterns = patterns('',
     url(r'^login/$', views.login, name='login'),
     url(r'^home/$', views.home, name='home'),
-    url(r'^profile$', views.profile, name='profile'),
-    url(r'^article$', views.article, name='article')
+    url(r'^profile/(?P<user_id>\d+)/$', views.profile, name='profile'),
+    url(r'^article/(?P<article_id>\d+)/$', views.article, name='article')
 )
--- tracker/urls.py
+++ tracker/urls.py
@@ -14,7 +14,6 @@ urlpatterns = patterns('',
 
 
     url(r'^main/', include('main.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
